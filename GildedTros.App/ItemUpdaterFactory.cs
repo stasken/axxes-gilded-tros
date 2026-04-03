46d8b68f1978365b16378d98a314e4239791447c
Stasken <arno_stas@hotmail.com>
2026-04-03 12:58:55 +0200

    added factory for updaters

---
+++ GildedTros.App/ItemUpdaterFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace GildedTros.App
+{
+    public static class ItemUpdaterFactory
+    {
+        private static readonly string[] SmellyItems =
+        {
+            "Duplicate Code",
+            "Long Methods",
+            "Ugly Variable Names"
+        };
+
+        public static ItemUpdater Create(Item item)
+        {
+            return item.Name switch
+            {
+                "Good Wine" => new GoodWineUpdater(item),
+
+                "B-DAWG Keychain" => new LegendaryItemUpdater(item),
+
+                var name when name.Contains("Backstage passes")
+                    => new BackstagePassUpdater(item),
+
+                var name when SmellyItems.Contains(name)
+                    => new SmellyItemUpdater(item),
+
+                _ => new NormalItemUpdater(item)
+            };
+        }
+    }
+}
+
