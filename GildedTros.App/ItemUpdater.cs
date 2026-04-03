1e025265dc020d150247802ad4be6dd9a9d48cd2
Stasken <arno_stas@hotmail.com>
2026-04-03 11:33:49 +0200

    create abstract updater base class

---
+++ GildedTros.App/ItemUpdater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace GildedTros.App
+{
+	public abstract class ItemUpdater
+	{
+		protected readonly Item Item;
+
+		protected ItemUpdater(Item item)
+		{
+			Item = item;
+		}
+
+		public void Update()
+		{
+			UpdateQuality();
+			UpdateSellIn();
+        }
+
+        protected virtual void UpdateSellIn() => Item.SellIn--;
+
+        protected abstract void UpdateQuality();
+
+    }
+}
+
